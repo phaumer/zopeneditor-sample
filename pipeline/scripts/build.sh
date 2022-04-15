d945835b025eb7d6af5d50c1b7278b41fa3489c9
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2022-04-15 10:04:22 +0200

    Pipeline aaS Initialization

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -5,5 +5,9 @@
 # $WORKSPACE is shared between steps
 source $WORKSPACE/virtual/environment/bin/activate
 
+# Just for test (MUST NOT BE USED EXTERNALY WE MUST USE A SSH KEY)
+wget https://rpmfind.net/linux/centos/8-stream/AppStream/x86_64/os/Packages/sshpass-1.09-4.el8.x86_64.rpm
+rpm -i sshpass-1.09-4.el8.x86_64.rpm
+
 cd pipeline/playbooks
 ansible-playbook -i inventories build.yml -e app_repo=$APP_REPO -e app_branch=$BRANCH
--- pipeline/scripts/setup.sh
+++ pipeline/scripts/setup.sh
@@ -13,10 +13,6 @@ pip3 install ansible==2.9.11
 # Install xmltodict required for the z/OS collection ibm.ibm_zos_cics
 pip3 install xmltodict 
 
-# Just for test (MUST NOT BE USED EXTERNALY WE MUST USE A SSH KEY)
-wget https://rpmfind.net/linux/centos/8-stream/AppStream/x86_64/os/Packages/sshpass-1.09-4.el8.x86_64.rpm
-rpm -i sshpass-1.09-4.el8.x86_64.rpm
-
 # Install RedHat Ansible collections for z/OS 
 ansible-galaxy collection install ibm.ibm_zos_core
 ansible-galaxy collection install ibm.ibm_zos_cics
